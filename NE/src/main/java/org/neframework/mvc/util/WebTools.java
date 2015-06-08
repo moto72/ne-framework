d47bc2c95966c7767d69ea71fa71f4710abed31f
fly2world <fly2world@qq.com>
2015-06-08 09:32:31 +0800

    获取来访者 IP 地址

--- NE/src/main/java/org/neframework/mvc/util/WebTools.java
+++ NE/src/main/java/org/neframework/mvc/util/WebTools.java
@@ -44,20 +44,6 @@ public class WebTools {
 		return webUrl.endsWith(".jsp") ? false : true;
 	}
 
-	public static String getIpAddr(HttpServletRequest request) {
-		String ip = request.getHeader("x-forwarded-for");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("WL-Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getRemoteAddr();
-		}
-		return ip;
-	}
-
 	public static void main(String[] args) {
 		String[] url = new String[] { "//xx///a.htm", "///////xx///a.htm" };
 		for (String u : url) {
---
+++ NE/src/main/java/org/neframework/mvc/util/ip/IPTools.java
@@ -0,0 +1,21 @@
+package org.neframework.mvc.util.ip;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IPTools {
+
+	public static String getIp(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
+
+}
